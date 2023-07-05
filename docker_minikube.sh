6da58bf1f42a2b3b9528b28ddf532efeb4ffbe39
AWSENG <116598374+AWSENG@users.noreply.github.com>
2023-07-05 23:16:44 +0500

    Update docker_minikube.sh

--- docker_minikube.sh
+++ docker_minikube.sh
@@ -1,9 +1,18 @@
+#!/bin/bash
 
-  sudo su -
+# Install Docker
 sudo apt update && apt -y install docker.io
- curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl &&   chmod +x ./kubectl && sudo mv ./kubectl /usr/local/bin/kubectl
+
+# Install kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl &&   chmod +x ./kubectl && sudo mv ./kubectl /usr/local/bin/kubectl
+
+# Install minikube
 curl -Lo minikube https://github.com/kubernetes/minikube/releases/download/v1.24.0/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+
+# Install conntrack
 apt install conntrack
+
+# Install CRI-Dockerd
 git clone https://github.com/Mirantis/cri-dockerd.git
 wget https://storage.googleapis.com/golang/getgo/installer_linux
 chmod +x ./installer_linux
@@ -19,9 +28,14 @@ sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/syst
 systemctl daemon-reload
 systemctl enable cri-docker.service
 systemctl enable --now cri-docker.socket
+
+# Install crictl
 wget https://github.com/kubernetes-sigs/cri-tools/releases/download/v1.24.0/crictl-v1.24.0-linux-amd64.tar.gz
 sudo tar zxvf crictl-v1.24.0-linux-amd64.tar.gz -C /usr/local/bin
 rm -f crictl-v1.24.0-linux-amd64.tar.gz
-minikube start --vm-driver=none
-minikube status
 
+# Start minikube
+minikube start --vm-driver=none
+
+# Check minikube status
+minikube status
